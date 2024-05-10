b0d96e18b2b5403db1410e90c45d6bc58a378d06
ME-SKY <meskalitofly@gmail.com>
2024-05-11 02:05:20 +1000

    Add settings store properties

--- src/store/settings.js
+++ src/store/settings.js
@@ -3,16 +3,32 @@ import {ref, computed} from 'vue';
 
 export const useSettingsStore = defineStore('settings', () => {
   // State
-  const settings = ref({});
+  // const settings = ref({});
+  const limit = ref(10);
+  const pair = ref('BTCUSDT');
+  const pairHistory = ref(['BTCUSDT']);
+  // const pairSwithingHistory = ref(false);
+
+
 
   // Actions
-  function updateSetting(key, value) {
-    settings.value[key] = value;
+  // function updateSetting(key, value) {
+  //   settings.value[key] = value;
+  // }
+
+  function switchPair(pair) {
+    settings.value.pair = pair;
+    updateHistory(pair);
+  }
+
+  function updateHistory(pair) {
+    pairHistory.value.push(pair);
   }
 
   // Getters
   const getSetting = (key) => settings.value[key];
+  // const pairHistory = computed(() => pairHistory.value ? );
 
   // Return the store interface
-  return { settings, updateSetting, getSetting };
+  return { limit, pair, pairHistory, switchPair};
 });
