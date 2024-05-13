332347fbac2f5640ca9ca7eba753cd6803596c6d
ME-SKY <meskalitofly@gmail.com>
2024-05-13 12:58:37 +1000

    Add firebase

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1whO_JtwrJzht3sOAFnvKccjqRUQYQo8",
+  authDomain: "orderbook-963fd.firebaseapp.com",
+  projectId: "orderbook-963fd",
+  storageBucket: "orderbook-963fd.appspot.com",
+  messagingSenderId: "1073100151573",
+  appId: "1:1073100151573:web:cab79a52b0b7b6e0f1271c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 // import Vue from 'vue'
